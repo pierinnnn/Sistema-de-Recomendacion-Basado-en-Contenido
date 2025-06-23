86cd5427ab33ba4a8cc4515c31c0b09587d10e22
PieroSanchez <pierosancas@gmail.com>
2025-06-22 21:29:49 -0500

    cambio en el modelo para mejora significativa

--- recomendacion.py
+++ recomendacion.py
@@ -9,9 +9,10 @@ st.set_page_config('Encuentra tu Pelicula!', layout='wide')
 def cargar_datos():
     df = pickle.load(open("modelo/movie_list.pkl", "rb"))
     similarity = pickle.load(open("modelo/similarity.pkl", "rb"))
-    return df, similarity
+    kmeans_model = pickle.load(open("modelo/kmeans_model.pkl", "rb"))
+    return df, similarity, kmeans_model
 
-df, similarity = cargar_datos()
+df, similarity, kmeans_model = cargar_datos()
 
 url_base_poster = "https://image.tmdb.org/t/p/w500"
 
@@ -20,16 +21,21 @@ def obtener_poster(poster_path):
         return "https://via.placeholder.com/500x750?text=No+Image"
     return url_base_poster + poster_path
 
-def recomendar(titulo, df, similarity, top_k=5):
+def recomendar_clustered(titulo, df, similarity, kmeans_model, top_k=5):
     indices = df[df['title'].str.lower() == titulo.lower()].index
 
     if len(indices) == 0:
         return None
 
     idx = indices[0]
-    sim_scores = list(enumerate(similarity[idx]))
+    cluster = df.iloc[idx]['cluster']
+
+    # Películas del mismo cluster
+    cluster_indices = df[df['cluster'] == cluster].index
+
+    sim_scores = [(i, similarity[idx][i]) for i in cluster_indices if i != idx]
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:top_k+1]  # ignorar la peli original
+    sim_scores = sim_scores[:top_k]
 
     recomendadas = []
     for i in sim_scores:
@@ -40,12 +46,12 @@ def recomendar(titulo, df, similarity, top_k=5):
         })
     return recomendadas
 
+
 #Interfaz
 st.title("🎬 Recomendador de Películas")
 df["title"] = df["title"].astype(str)
 df["title"] = df["title"].str.replace(r"[\n\r\t]+", " ", regex=True)
 df["title"] = df["title"].str.strip()
-df = df.drop_duplicates(subset="title")
 df["title"] = df["title"].apply(lambda x: x[:60] + "..." if len(x) > 60 else x)
 titulo_seleccionado = st.selectbox(
     "Elige una película",
@@ -53,7 +59,7 @@ titulo_seleccionado = st.selectbox(
 )
 
 if st.button("Recomendar"):
-    resultados = recomendar(titulo_seleccionado, df, similarity)
+    resultados = recomendar_clustered(titulo_seleccionado, df, similarity, kmeans_model)
 
     if resultados:
         st.subheader(f"🎯 Películas similares a: *{titulo_seleccionado}*")
